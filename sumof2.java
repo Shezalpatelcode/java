1cf708767c68931e38a312edc82947a0f0b3dd47
Shezalpatelcode <sheezalp88@gmail.com>
2025-04-08 12:55:57 +0530

    Java file

---
+++ sumof2.java
@@ -0,0 +1,10 @@
+//wap to print the sum of two numbers 
+public class sumof2 {
+    public static void main(String[] args) {
+        int a =5;
+        int b =10;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
+
